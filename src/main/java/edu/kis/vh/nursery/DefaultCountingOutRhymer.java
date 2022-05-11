2da21ae9ba11851554e9ed43f4a415cec0f733ae
Juliusz Brodnicki <juliusbrodnicki@gmail.com>
2022-05-11 02:22:11 +0200

    1.5 Add Comments

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,32 +2,66 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
+	/**
+	 * ERROR_RETURN_VALUE - wartość zwraca w razie gdy tablica nie zawiera rymów
+	 */
 	private static final int ERROR_RETURN_VALUE = -1;
+	/**
+	 * TOTAL_INIT_VALUE - wartość inicjalizująca tablice rymów
+	 */
 	private static final int TOTAL_INIT_VALUE = -1;
+	/**
+	 * MAX_ARRAY_SIZE - maksymalny rozmiar tablicy 
+	 */
 	private static final int MAX_ARRAY_SIZE = 12;
+	/**
+	 * MAX_ELEMENTS_COUNT - maksymalny index elementu tablicy, zależy od wartości MAX_ARRAY_SIZE
+	 */
 	private static final int MAX_ELEMENTS_COUNT = MAX_ARRAY_SIZE - 1;
 
+	/**
+	 * numbers - zmienna tablicowa przechowująca rymy
+	 */
 	private final int[] numbers = new int[MAX_ARRAY_SIZE];
 
+	/**
+	 * total - wartość przechowuję obecną liczbę rymów
+	 */
 	private int total = TOTAL_INIT_VALUE;
 
+	/**
+	 * @return metoda zwraca obecną liczbę rymów
+	 */
 	public int getTotal() {
 		return total;
 	}
 
+	/**
+	 * @param in - liczba rymów
+	 * Metoda metoda zapisuję liczbę rymów podanych w parametrze do tablicy numbers
+	 */
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
+	/**
+	 * @return metoda zwraca true jeżeli tablica rymów jest pusta lub false w przeciwnym razie 
+	 */
 	public boolean callCheck() {
 		return total == TOTAL_INIT_VALUE;
 	}
 
+	/**
+	 * @return metoda zwraca true jeżeli tablica rymów jest pełna lub false w przeciwnym razie
+	 */
 	public boolean isFull() {
 		return total == MAX_ELEMENTS_COUNT;
 	}
 
+	/**
+	 * @return metoda zwraca liczbę rymów ostatnio zapisaną do tablicy numbers
+	 */
 	protected int peekaboo() {
 		if (callCheck()) {
 			return ERROR_RETURN_VALUE;
@@ -35,6 +69,10 @@ public class DefaultCountingOutRhymer {
 		return numbers[total];
 	}
 
+	/**
+	 * @return metoda zwraca ostatnią liczbę rymów zapisaną do tablicy jednocześnie usuwając te wartość z tablicy
+	 * w razie gdy tablica jest pusta zwraca wartość ERROR_RETURN_VALUE
+	 */
 	public int countOut() {
 		if (callCheck()) {
 			return ERROR_RETURN_VALUE;
